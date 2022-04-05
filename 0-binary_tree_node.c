7eaaf599061aff7f25cad13bcad1c3bc83f4bb02
Ronroeandassociates <3321@holbertonschool.com>
2022-04-05 11:39:08 -0500

    fixed: documentation of binary_tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,8 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - that inserts a node as the left-child
- * of another node
+ * binary_tree_node - creates a binary tree node
  * @parent: is a pointer to the node to insert the left-child in
  * @value: is the value to store in the new node
  *
