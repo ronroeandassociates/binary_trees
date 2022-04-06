e595c8bf36aa48b1f2454693a615221a6ccdd5a9
Ronroeandassociates <3321@holbertonschool.com>
2022-04-06 09:07:56 -0500

    Task: 9. Height
    a function that measures the height of a binary tree
    
    Prototype: size_t binary_tree_height(const binary_tree_t *tree);
    Where tree is a pointer to the root node of the tree to measure the height.
    If tree is NULL, your function must return 0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,17 +1,48 @@
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
-};
+#include "binary_trees.h"
 
-typedef struct binary_tree_s binary_tree_t;
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ *
+ * Return: the size of the height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height = 0;
+
+	if (tree == NULL)
+		return (0);
+	aux_height(tree, 0, &height);
+	return (height);
+}
+
+/**
+ * aux_height - recursive call
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ * @node_h: node height
+ * @height: height
+ */
+void aux_height(const binary_tree_t *tree, size_t node_h, size_t *height)
+{
+	if (tree->left)
+		aux_height(tree->left, node_h + 1, height);
+	if (tree->right)
+		aux_height(tree->right, node_h + 1, height);
+	if (is_leaf(tree) && node_h > *height)
+		*height = node_h;
+}
+
+/**
+ * is_leaf - checks if a node is a leaf
+ * @node: node to be checked
+ *
+ * Return: 1 if node is a leaf. 0 otherwise or in case of error
+ */
+int is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
