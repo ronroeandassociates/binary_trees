9063131b0d4c948671daccad109ead9b40792bbc
Ronroeandassociates <3321@holbertonschool.com>
2022-04-05 19:22:21 -0500

    Task: 3. Delete
    a function that deletes an entire binary tree
    
    Prototype: void binary_tree_delete(binary_tree_t *tree);
    Where tree is a pointer to the root node of the tree to delete
    If tree is NULL, do nothing

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,17 +1,41 @@
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
-};
+#include "binary_trees.h"
 
-typedef struct binary_tree_s binary_tree_t;
+/**
+ * binary_tree_insert_right - function that inserts a node as the
+ * right-child of another node
+ * @parent: is a pointer to the node to insert the right-child in
+ * @value: is the value to store in the new node
+ *
+ * Return: a pointer to the created node,
+ * or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+	binary_tree_t *aux;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	if (parent->right == NULL)
+	{
+		parent->right = new_node;
+		new_node->right = NULL;
+	}
+	else
+	{
+		aux = parent->right;
+		aux->parent = new_node;
+		parent->right = new_node;
+		new_node->right = aux;
+	}
+	return (new_node);
+}
