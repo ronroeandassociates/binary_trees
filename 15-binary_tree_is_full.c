f67651a54e291633c4dc1eefa0fd97f3fbe10789
Ronroeandassociates <3321@holbertonschool.com>
2022-04-06 12:34:51 -0500

    Task: 15 Is full
    Write a function that checks if a binary tree is full
    
    Prototype: int binary_tree_is_full(const binary_tree_t *tree);
    Where tree is a pointer to the root node of the tree to check
    If tree is NULL, your function must return 0

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,17 +1,17 @@
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
-};
 
-typedef struct binary_tree_s binary_tree_t;
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if full. 0 otherwise or if tree is not full
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+}
