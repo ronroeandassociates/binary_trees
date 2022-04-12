4f41d17eebe3b877d7081d7889d4ac36c72c3995
Ronroeandassociates <3321@holbertonschool.com>
2022-04-12 09:36:28 -0500

    Task: 0 Is BST
    a function that checks if a binary tree is a valid Binary Search Tree
    
    Prototype: int binary_tree_is_bst(const binary_tree_t *tree);
    Where tree is a pointer to the root node of the tree to check
    Your function must return 1 if tree is a valid BST, and 0 otherwise
    If tree is NULL, return 0
    Properties of a Binary Search Tree:
    
    The left subtree of a node contains only nodes with values less than the node’s value
    The right subtree of a node contains only nodes with values greater than the node’s value
    The left and right subtree each must also be a binary search tree
    There must be no duplicate values

--- binary_trees.h
+++ binary_trees.h
@@ -21,6 +21,7 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -41,6 +42,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+int binary_tree_is_bst(const binary_tree_t *tree);
 
 /* auxiliary function */
 void aux_height(const binary_tree_t *tree, size_t node_h, size_t *height);
