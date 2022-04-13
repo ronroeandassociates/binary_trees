f767bdd1583e877bb0174195465d480c95127bbf
Ronroeandassociates <3321@holbertonschool.com>
2022-04-13 10:55:33 -0500

    Task: 2. BST - Array to BST
    a function that builds a Binary Search Tree from an array
    
    Prototype: bst_t *array_to_bst(int *array, size_t size);
    Where array is a pointer to the first element of the array to be converted
    And size is the number of element in the array
    Your function must return a pointer to the root node of the created BST, or NULL on failure
    If a value of the array is already present in the tree, this value must be ignored

--- binary_trees.h
+++ binary_trees.h
@@ -44,6 +44,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
 
 /* auxiliary function */
 void aux_height(const binary_tree_t *tree, size_t node_h, size_t *height);
